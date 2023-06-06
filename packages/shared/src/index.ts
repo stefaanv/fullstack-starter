b873799ffc46f686fd7d858befedae92731d05a1
Stefaan Vandevelde <stefaan.vandevelde@bruyland.be>
2023-06-06 09:24:10 +0200

    Error handling added

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,4 @@
 import { FrontendConfigDto } from './dtos/frontend-settings.dto'
 import { AppInfoDto } from './dtos/app-info.dto'
-export { FrontendConfigDto, AppInfoDto }
+import { ensureError, to, toSync } from './error-handling-helpers'
+export { FrontendConfigDto, AppInfoDto, to, toSync, ensureError }
