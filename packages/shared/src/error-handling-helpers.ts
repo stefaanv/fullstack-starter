bbea772f627072caf55b845043ddc2a8d1233883
Stefaan Vandevelde <stefaan.vandevelde@bruyland.be>
2023-06-08 10:13:45 +0200

    Improvement in error handling

--- packages/shared/src/error-handling-helpers.ts
+++ packages/shared/src/error-handling-helpers.ts
@@ -1,23 +1,41 @@
-export function ensureError(value: unknown): Error {
-  if (value instanceof Error) return value
+export function ensureError<E = Error>(value: unknown): E {
+  if (value instanceof Error) return value as E
 
   let stringified = '[Unable to stringify the thrown value]'
   try {
     stringified = JSON.stringify(value)
   } catch {}
 
-  return new Error(stringified)
+  return new Error(stringified) as E
 }
 
-export async function to<T>(promise: Promise<T>): Promise<[null | Error, T | undefined]> {
-  return promise.then(res => [null, res] as [null, T]).catch(e => [ensureError(e), undefined])
+export type FailedTuple<E = Error> = [E, undefined]
+export type SuccessTuple<T> = [null, T]
+export type FailedOrSuccessTuple<T, E = Error> = FailedTuple<E> | SuccessTuple<T>
+export type handleErrorFn<T, E = Error> = (error: E) => FailedOrSuccessTuple<T, E>
+const processCatch = <T, E = Error>(e: unknown, handleError?: handleErrorFn<T, E>) => {
+  if (handleError) return handleError(ensureError(e))
+  return [ensureError(e), undefined] as FailedOrSuccessTuple<T, E>
 }
 
-export function toSync<T>(canThrow: () => T): [null | Error, T | undefined] {
+export async function to<T, E = Error>(
+  promise: Promise<T>,
+  handleError?: handleErrorFn<T, E>,
+): Promise<FailedOrSuccessTuple<T, E>> {
+  return promise
+    .then((result: T) => [null, result] as SuccessTuple<T>)
+    .catch((e: unknown) => processCatch<T, E>(e, handleError))
+}
+
+export function toSync<T>(
+  canThrow: () => T,
+  handleError?: handleErrorFn<T>,
+): [null | Error, T | undefined] {
   try {
     return [null, canThrow()]
-  } catch (error) {
-    return [ensureError(error), undefined]
+  } catch (e: unknown) {
+    if (handleError) return handleError(ensureError(e))
+    return [ensureError(e), undefined]
   }
 }
 
